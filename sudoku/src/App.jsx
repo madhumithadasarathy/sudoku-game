57d905ef29dff9491977c5a7f2a3f504fce370e9
madhumithadasarathy <madhumithadasarathy@gmail.com>
2025-04-19 11:14:51 +0530

    Created LevelMap.jsx and updated App.jsx

--- sudoku/src/App.jsx
+++ sudoku/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import SudokuGame from './SudokuGame'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './Welcome';
+import LevelMap from './LevelMap';
+import SudokuGame from './SudokuGame';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <SudokuGame />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/levels" element={<LevelMap />} />
+        <Route path="/game/:level" element={<SudokuGame />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
