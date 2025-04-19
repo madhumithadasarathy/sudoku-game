e1cb387283cc425983dc92280f8ff26e4d5d6782
madhumithadasarathy <madhumithadasarathy@gmail.com>
2025-04-19 11:20:52 +0530

    Added new animations to tailwind.config.js

--- sudoku/tailwind.config.js
+++ sudoku/tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spaceship-move': 'spaceshipMove 6s linear infinite',
+        'stars-twinkle': 'twinkleStars 1.5s infinite',
+        'fade-in': 'fadeIn 2s ease-in-out forwards',
+      },
+      keyframes: {
+        spaceshipMove: {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-50px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        twinkleStars: {
+          '0%': { opacity: 0.2 },
+          '50%': { opacity: 0.7 },
+          '100%': { opacity: 0.2 },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
